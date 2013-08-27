c77fbede2a1bc5ea5518d2b653a20c0f3633b83b
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-08-27 19:55:45 +0300

    Added repo details page

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -27,10 +27,42 @@ function loadRepos() {
         var i, repo;
 		// foreach repo add li
         $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='http://github.com/" + repo.username + "/" + repo.name + "'>"
+            $("#allRepos").append("<li><a href='repo-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
             + "<h4>" + repo.name + "</h4>"
             + "<p>" + repo.username + "</p></a></li>");
         });
         $('#allRepos').listview('refresh');
     });
+}
+
+
+/////////////////////////////////
+// User in the repo details page
+/////////////////////////////////
+$(document).on('pageshow', '#reposDetail', function(event) {
+	urlVars = getUrlVars();
+    var owner = urlVars.owner;
+    var name = urlVars.name;
+    loadRepoDetail(owner,name);
+});
+
+// load repo details
+function loadRepoDetail(owner,name) {
+	// Get Repo details
+    $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+		
+        var repo = data;
+		// Add repo name
+        $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+		// Add description
+        $('#description').text(repo.description);
+		// Add number of forks and watchers
+        $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
+		
+		// Owner details
+		// Owner avatar
+        $('#avatar').attr('src', repo.owner.avatar_url);
+		// Owner name
+        $('#ownerName').html("<strong>Owner:</strong> <a href='http://github.com/" + repo.owner.login + "'>" + repo.owner.login + "</a>");
+	});
 }
